5bd480267ac8c34434f8a3adc93c80f01d843d35
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-10-10 15:27:10 +0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-let tatitude, longitude = "";
+let latitude, longitude = "";
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+}
+else {
+    alert("Tarayıcınız konum bilgisini alamıyor .");
+}
+
+function onSuccess(position) {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+}
+
+function onError(error) {
+    if (error.code = 1) {
+
+        alert("Kullanıcı konum izni vermedi");
+    }
+
+    else if (error.code = 2) {
+        alert("Konum bilgisi alınamadı");
+    }
+
+    else {
+        alert("Bir hata oluştu");
+    }
+}
